c06a6b9618c25a23350e2aeb9554cab4814e1097
SangJin Lee <sangjin256@naver.com>
2019-11-02 11:09:48 +0900

    Create A+B.cs

---
+++ A+B.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+
+public class Lecture 
+{
+	public static void Main(string[] args) {
+		int[] a = new int[2];
+		a = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+		
+		Console.WriteLine(a[0] + a[1]);
+	}
+}
