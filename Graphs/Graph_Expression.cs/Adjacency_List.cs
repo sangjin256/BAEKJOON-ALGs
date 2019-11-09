cc389e9cc17e332e38f801f6ba1bf4f77d9817cb
SJGRID <sangjin256@naver.com>
2019-11-09 14:03:11 +0900

    Update Adjacency_List.cs

--- Graphs/Graph_Expression.cs/Adjacency_List.cs
+++ Graphs/Graph_Expression.cs/Adjacency_List.cs
@@ -7,42 +7,42 @@ using System.Collections.Generic;
 public class Lecture 
 {
 	//인접 리스트(Adjacency list)로 표현 (가중치 x)
-	static List<int>[] adj_l;
+	static List<int>[] adj;
 
 	//인접 리스트로 표현 (가중치 o)
-	static List<Tuple<int, int>>[] adj_lw;
+	static List<Tuple<int, int>>[] adj_w;
 	
 	public static void Main(string[] args) {
 		AdjacencyList(10, false);
-		adj_l[1].Add(2);
-		adj_l[2].Add(3);
-		adj_l[2].Add(4);
-		adj_l[3].Add(4);
-		adj_l[4].Add(1);
+		adj[1].Add(2);
+		adj[2].Add(3);
+		adj[2].Add(4);
+		adj[3].Add(4);
+		adj[4].Add(1);
 		
 		AdjacencyList(10, true);
         //(b,w)는 b로 가는 가중치가 w인 간선을 의미
-		adj_lw[1].Add(new Tuple<int,int>(2,5));
-		adj_lw[2].Add(new Tuple<int,int>(3,7));
-		adj_lw[2].Add(new Tuple<int,int>(4,6));
-		adj_lw[3].Add(new Tuple<int,int>(4,5));
-		adj_lw[4].Add(new Tuple<int,int>(1,2));
+		adj_w[1].Add(new Tuple<int,int>(2,5));
+		adj_w[2].Add(new Tuple<int,int>(3,7));
+		adj_w[2].Add(new Tuple<int,int>(4,6));
+		adj_w[3].Add(new Tuple<int,int>(4,5));
+		adj_w[4].Add(new Tuple<int,int>(1,2));
 	}
 	
 	//인접 리스트를 초기화해주는 함수. 가중치가 있는 그래프와 없는 그래프를 bool로 받아서 초기화한다.
 	public static void AdjacencyList(int length, bool isWeight){
 		if(isWeight == false){
-			adj_l = new List<int>[length];
+			adj = new List<int>[length];
 			
 			for(int i = 0; i < length; i++){
-				adj_l[i] = new List<int>();
+				adj[i] = new List<int>();
 			}
 		}
 		else {
-			adj_lw = new List<Tuple<int, int>>[length];
+			adj_w = new List<Tuple<int, int>>[length];
 			
 			for(int i = 0; i < length; i++){
-				adj_lw[i] = new List<Tuple<int, int>>();
+				adj_w[i] = new List<Tuple<int, int>>();
 			}
 		}
 	}
