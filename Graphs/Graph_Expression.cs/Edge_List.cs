b16fcee4470c93ddc08cbeb02c984872df7b19e3
SJGRID <sangjin256@naver.com>
2019-11-10 10:33:57 +0900

    Update Edge_List.cs

--- Graphs/Graph_Expression.cs/Edge_List.cs
+++ Graphs/Graph_Expression.cs/Edge_List.cs
@@ -7,22 +7,23 @@ using System.Collections.Generic;
 public class Lecture 
 {
 	//가중치가 없는 간선리스트
-	static List<Tuple<int,int>> edges = new List<Tuple<int,int>>();
+	//(int,int)방식은 c# 7.0부터 가능 그 전 버전은 Tuple<int,int,int>로 해줘야함
+	static List<(int,int)> edges = new List<(int,int)>();
 	//가중치가 있는 간선리스트
-	static List<Tuple<int,int,int>> edges_w = new List<Tuple<int,int,int>>();
+	static List<(int,int,int)> edges = new List<(int,int,int)>();
 	
 	public static void Main(string[] args) {
-		edges.Add(new Tuple<int,int>(1,2));
-		edges.Add(new Tuple<int,int>(2,3));
-		edges.Add(new Tuple<int,int>(2,4));
-		edges.Add(new Tuple<int,int>(3,4));
-		edges.Add(new Tuple<int,int>(4,1));
+		edges.Add((1,2));
+		edges.Add((2,3));
+		edges.Add((2,4));
+		edges.Add((3,4));
+		edges.Add((4,1));
 		
 		//(a,b,w)는 a에서 b로 가는 가중치가 w인 간선을 의미
-		edges_w.Add(new Tuple<int,int,int>(1,2,5));
-		edges_w.Add(new Tuple<int,int,int>(2,3,7));
-		edges_w.Add(new Tuple<int,int,int>(2,4,6));
-		edges_w.Add(new Tuple<int,int,int>(3,4,5));
-		edges_w.Add(new Tuple<int,int,int>(4,1,2));
+		edges_w.Add((1,2,5));
+		edges_w.Add((2,3,7));
+		edges_w.Add((2,4,6));
+		edges_w.Add((3,4,5));
+		edges_w.Add((4,1,2));
 	}
 }
