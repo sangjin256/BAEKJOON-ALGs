51ae76cea04ff48b74cc7c33fe5edc0d62c32a0d
SJGRID <sangjin256@naver.com>
2019-11-10 10:33:55 +0900

    Update Adjacency_List.cs

--- Graphs/Graph_Expression.cs/Adjacency_List.cs
+++ Graphs/Graph_Expression.cs/Adjacency_List.cs
@@ -10,7 +10,7 @@ public class Lecture
 	static List<int>[] adj;
 
 	//인접 리스트로 표현 (가중치 o)
-	static List<Tuple<int, int>>[] adj_w;
+	static List<(int, int)>[] adj_w;
 	
 	public static void Main(string[] args) {
 		AdjacencyList(10, false);
@@ -22,11 +22,11 @@ public class Lecture
 		
 		AdjacencyList(10, true);
         //(b,w)는 b로 가는 가중치가 w인 간선을 의미
-		adj_w[1].Add(new Tuple<int,int>(2,5));
-		adj_w[2].Add(new Tuple<int,int>(3,7));
-		adj_w[2].Add(new Tuple<int,int>(4,6));
-		adj_w[3].Add(new Tuple<int,int>(4,5));
-		adj_w[4].Add(new Tuple<int,int>(1,2));
+		adj_w[1].Add((2,5));
+		adj_w[2].Add((3,7));
+		adj_w[2].Add((4,6));
+		adj_w[3].Add((4,5));
+		adj_w[4].Add((1,2));
 	}
 	
 	//인접 리스트를 초기화해주는 함수. 가중치가 있는 그래프와 없는 그래프를 bool로 받아서 초기화한다.
@@ -39,10 +39,10 @@ public class Lecture
 			}
 		}
 		else {
-			adj_w = new List<Tuple<int, int>>[length];
+			adj_w = new List<(int, int)>[length];
 			
 			for(int i = 0; i < length; i++){
-				adj_w[i] = new List<Tuple<int, int>>();
+				adj_w[i] = new List<(int, int)>();
 			}
 		}
 	}
