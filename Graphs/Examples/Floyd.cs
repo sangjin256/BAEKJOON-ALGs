ea6f3f667182c2e3e9c147e17def70260debcf1b
SJGRID <sangjin256@naver.com>
2019-11-23 14:33:56 +0900

    Update Floyd.cs

--- Graphs/Examples/Floyd.cs
+++ Graphs/Examples/Floyd.cs
@@ -1,3 +1,13 @@
+/*
+# 11404
+n(1 ≤ n ≤ 100)개의 도시가 있다. 그리고 한 도시에서 출발하여 다른 도시에 도착하는
+
+m(1 ≤ m ≤ 100,000)개의 버스가 있다. 각 버스는 한 번 사용할 때 필요한 비용이 있다.
+
+모든 도시의 쌍 (A, B)에 대해서 도시 A에서 B로 가는데 필요한 비용의 최솟값을 구하는
+
+프로그램을 작성하시오.
+*/
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -6,7 +16,9 @@ public class Lecture
 	static List<(int,int)>[] adj;
 	static long[,] dist;
 	public static void Main(string[] args) {
+        //도시의 개수
         int n = int.Parse(Console.ReadLine());
+        //버스의 개수
         int k = int.Parse(Console.ReadLine());
         adj = new List<(int,int)>[n+1];
         for(int i = 0; i <= n; i++){
