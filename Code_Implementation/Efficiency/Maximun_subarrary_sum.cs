f15cb972976e9bfa37ee0c6f7c14e3392594e50d
SJGRID <sangjin256@naver.com>
2019-11-30 11:10:40 +0900

    Create Maximun_subarrary_sum.cs

---
+++ Code_Implementation/Efficiency/Maximun_subarrary_sum.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Lecture 
+{
+	public static void Main(string[] args) {
+        int[] arr = {-1,2,4,-3,5,2,-5,2};
+        int best = 0;
+        int sum = 0;
+        for(int i = 0; i < arr.Length; i++){
+            sum = Math.Max(arr[i], sum+arr[i]);
+            best = Math.Max(sum,best);
+        }
+        Console.WriteLine(best);
+    }
+}
