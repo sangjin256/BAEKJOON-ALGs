c2a945280b7f9b505f7cd690d6f401fa37bfbec9
SangJin Lee <sangjin256@naver.com>
2019-12-05 16:22:04 +0900

    Update Snail_Want_Go_Up.cs

--- Math/Examples/Snail_Want_Go_Up.cs
+++ Math/Examples/Snail_Want_Go_Up.cs
@@ -14,7 +14,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Lecture 
+public class Lecture
 {
 	public static void Main(string[] args) {
         // A, B, V
