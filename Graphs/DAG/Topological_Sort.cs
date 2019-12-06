e7be74b5937799591dfb96a01e2c86dd971e7aed
SJGRID <sangjin256@naver.com>
2019-12-06 14:15:52 +0900

    Update Topological_Sort.cs

--- Graphs/DAG/Topological_Sort.cs
+++ Graphs/DAG/Topological_Sort.cs
@@ -9,6 +9,9 @@ public class Lecture
 {
 	//인접 리스트(Adjacency list)로 표현
 	static List<int>[] adj;
+	
+	//위상정렬이 가능한지 체크
+	static bool Impossible = false;
 
     static List<int> topol = new List<int>();
 
@@ -30,19 +33,32 @@ public class Lecture
 		
 		topol.Reverse();
 		
+		if(Impossible){
+        	Console.WriteLine("사이클 존재 -> 위상정렬 불가");
+        	return;
+		}
+		
         foreach(var c in topol){
             Console.WriteLine(c + " ");
         }
     }
-
+    //사이클이 있으면 위상정렬 불가 그 판단을 위한 bool배열
+    //처리되는 중인 상태의 노드를 만나게 되면 사이클이 존재한다는 뜻
+    static bool[] processing = new bool[7];
     static bool[] visited = new bool[7];
     public static void Dfs(int s){
         if(visited[s]) return;
         visited[s] = true;
+        processing[s] = true;
         foreach(var u in adj[s]){
             Dfs(u);
+            if(processing[u]){
+            	Impossible = true;
+                break;
+            }
         }
         //처리가 끝난 노드를 리스트에 넣어준다.
         topol.Add(s);
+        processing[s] = false;
     }
 }
