94b9f291c7c1b438ce09b96228f4d9114a1c6fb2
SJGRID <sangjin256@naver.com>
2019-12-07 16:54:38 +0900

    Create Sliding_Window.cs

---
+++ Code_Implementation/Amortized Analysis/Sliding_Window.cs
@@ -0,0 +1,19 @@
+//슬라이딩 윈도 : 어떤 배열에 대해서 왼쪽에서 오른쪽으로 움직여 나가는 고정된 크기의
+//부분 배열을 말한다.
+//이 슬라이딩 윈도의 최솟값을 관리하는 문제
+//Nearest_Smaller_Elements와 유사한 방법을 사용
+//슬라이딩 윈도의 크기를 4로 가정
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+//덱을 사용해야 하므로 덱 구현 후 시도해볼 것!!
+//p.138
+public class Lecture 
+{
+	public static void Main(string[] args) {
+        return;
+    }
+}
