9dc99a5196524fc3b070eacecbed30d2fcb20311
SJGRID <sangjin256@naver.com>
2019-12-08 18:58:14 +0900

    Update Range_Multiply.cs

--- Code_Implementation/Query/Examples/Range_Multiply.cs
+++ Code_Implementation/Query/Examples/Range_Multiply.cs
@@ -19,7 +19,7 @@ using System.Linq;
 public class Lecture 
 {
     //구간 트리
-    static long[] tree;
+    static ulong[] tree;
 	public static void Main(string[] args) {
         int[] tem = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
         //구간 트리의 크기를 결정하기 전에 arr배열의 크기보다 크거나 같은 길이 n을 구함
@@ -30,21 +30,21 @@ public class Lecture
                     break;
                 }
             }
-            tree = new long[n*2];
+            tree = new ulong[n*2];
         }
         else{
             n = tem[0];
-            tree = new long[n*2];
+            tree = new ulong[n*2];
         }
 
         //원래 배열 값 받기
         for(int i = 0; i < tem[0]; i++){
-            tree[i+tree.Length/2] = long.Parse(Console.ReadLine()) % 1000000007;
+            tree[i+tree.Length/2] = ulong.Parse(Console.ReadLine()) % 1000000007;
         }
 
         //구간 트리 전처리
         for(int i = tree.Length/2-1; i>= 1; i--){
-            tree[i] = tree[i*2] * tree[i*2+1] % 1000000007;
+            tree[i] = (tree[i*2] * tree[i*2+1]) % 1000000007;
         }
 		
         //a,b,c가 주어질 때 a가 1이면 b번째 수를 c로 바꾸고 a가 2이면 b~c곱 구하기
@@ -59,10 +59,10 @@ public class Lecture
         }
     }
 
-    public static long Multiply(int a, int b, int n){
+    public static ulong Multiply(int a, int b, int n){
         a += n;
         b += n;
-        long s = 1;
+        ulong s = 1;
         while(a <= b){
             if(a%2==1) s *= tree[a++] % 1000000007;
             if(b%2==0) s *= tree[b--] % 1000000007;
@@ -73,9 +73,9 @@ public class Lecture
 
     public static void Change(int k, int x, int n){
         k += n;
-        tree[k] = x;
+        tree[k] = (ulong)x % 1000000007;
         for(k/=2; k>=1; k/=2){
-            tree[k] = tree[k*2] * tree[k*2+1] % 1000000007;
+            tree[k] = (tree[k*2] * tree[k*2+1]) % 1000000007;
         }
     }
 }
