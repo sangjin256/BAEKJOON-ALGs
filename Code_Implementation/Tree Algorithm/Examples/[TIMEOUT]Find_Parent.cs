364efc92c8f2042c4834fb07b7340dc646cf689f
SangJin Lee <sangjin256@naver.com>
2020-01-03 16:29:49 +0900

    Update [TIMEOUT]Find_Parent.cs

--- Code_Implementation/Tree Algorithm/Examples/[TIMEOUT]Find_Parent.cs
+++ Code_Implementation/Tree Algorithm/Examples/[TIMEOUT]Find_Parent.cs
@@ -14,52 +14,52 @@ public class Lecture
 {
     static List<int>[] adj;
 	public static void Main(string[] args) {
+		
+        //초기화
         int n = int.Parse(Console.ReadLine());
-        Adj_Initializaion(n);
+        AdjacentInit(n);
+        
         for(int i = 1; i < n; i++){
             int[] temp = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
-            Adj_Add(temp[0],temp[1]);
+            Add(temp[0], temp[1]);
         }
-
+        
         parent = new int[n+1];
-        visited = new int[n+1];
-		
-		Bfs(1);
-
-        for(int i = 2; i < parent.Length; i++){
+        visited = new bool[n+1];
+        Bfs();
+        for(int i = 2; i < n+1; i++){
             Console.WriteLine(parent[i]);
         }
     }
 
-    //각 노드별로 부모노드를 담는 배열을 생성한다.
+    //부모 노드의 번호를 넣을 배열을 만들어준다.
     static int[] parent;
     static bool[] visited;
-
-    //bfs를 활용하여 부모노드 번호를 출력한다.
-    public static void Bfs(int x){
+    
+    //Bfs로 순회한다.
+    public static void Bfs(){
         Queue<int> q = new Queue<int>();
-        q.Push(x);
-        visited[x] = true;
+        q.Enqueue(1);
+        visited[1] = true;
         while(q.Count != 0){
             int s = q.Dequeue();
             foreach(var u in adj[s]){
                 if(visited[u]) continue;
-                visited[u] = true;
                 parent[u] = s;
+                visited[u] = true;
                 q.Enqueue(u);
             }
         }
     }
-    //인접 리스트 초기화
-    public static void Adj_Initializaion(int n){
+
+    public static void AdjacentInit(int n){
         adj = new List<int>[n+1];
-        for(int i = 1; i < adj.Length; i++){
+        for(int i = 1; i < n+1; i++){
             adj[i] = new List<int>();
         }
     }
 
-    //양방향 그래프이므로 양쪽 다 받아준다.
-    public static void Adj_Add(int a, int b){
+    public static void Add(int a, int b){
         adj[a].Add(b);
         adj[b].Add(a);
     }
