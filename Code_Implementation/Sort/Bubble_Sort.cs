e241fff6306af58970cdee9cbf3d9e55c6f53e1d
SJGRID <sangjin256@naver.com>
2020-01-05 15:33:31 +0900

    Update Bubble_Sort.cs

---
+++ Code_Implementation/Sort/Bubble_Sort.cs
@@ -0,0 +1,37 @@
+//O(n2)
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Lecture 
+{
+    static int[] a;
+	public static void Main(string[] args) {
+        a = new int[]{5,4,7,1,9,2};
+        BubbleSort(a,a.Length);
+        for(int i = 0; i < a.Length; i++){
+            Console.Write(a[i]);
+        }
+    }
+
+    static void BubbleSort(int[] a, int n){
+        int k = 0;
+        while(k < n-1){
+            int last = n-1;
+            for(int i = n-1; i > k; i--){
+                if(a[i-1]>a[i]){
+                    Swap(a,i-1,i);
+                    last = i;
+                }
+            }
+            k = last;
+        }
+    }
+
+    static void Swap(int[] a, int x, int y){
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+}
