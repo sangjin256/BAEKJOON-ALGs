c70a9909bc023d8950bf036804e43a3ddc8ca6f3
SJGRID <sangjin256@naver.com>
2020-01-05 15:33:25 +0900

    Update Partial_Lattice_Count.cs

--- Code_Implementation/Bit-parallel algorithm/Partial_Lattice_Count.cs
+++ Code_Implementation/Bit-parallel algorithm/Partial_Lattice_Count.cs
@@ -2,6 +2,7 @@
 //n*n크기의 격자가 있는데, 각 지점에는 검은색(1) 또는 흰색(0)이 칠해져 있다.
 //이때 네 귀퉁이에 모두 검은색이 칠해져 있는 부분 격자의 개수를 구하려 한다.
 //c++의 비트셋이 없으므로 O(n3)이 나온다.
+//그래도 무차별알고리즘으로 하는것보다는 비트를 쓰는게 좀더 빠르다는 결론이 나옴
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Linq;
 
 public class Lecture 
 {
+    //bitarray로 만들어도 됨(더 빠른지는 아직 미확인)
     static int[] row = {Convert.ToByte("01001",2),
                         Convert.ToByte("01100",2),
                         Convert.ToByte("10000",2),
