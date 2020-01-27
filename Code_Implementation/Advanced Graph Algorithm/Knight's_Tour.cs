d4a72093da25790632e2ae2e11da95cdc0961a71
SJGRID <sangjin256@naver.com>
2020-01-27 10:35:00 +0900

    Update Knight's_Tour.cs

--- Code_Implementation/Advanced Graph Algorithm/Knight's_Tour.cs
+++ Code_Implementation/Advanced Graph Algorithm/Knight's_Tour.cs
@@ -20,12 +20,19 @@ public class Lecture
     
     static int[] dx = new int[]{-1, -1, 1, 1, -2, -2, 2, 2};
     static int[] dy = new int[]{2, -2, 2, -2, 1, -1, 1, -1};
+    
+    //배열의 index에는 현재 위치의 좌표가 들어가고 그 좌표의 리스트에는
+    //갈수 있는 좌표와 그 좌표에서 갈 수 있는 위치의 개수가 들어간다.
+    static List<(int,int,int)>[,] list;
+    
     public static void Main(string[] args) {
         //5*5체스판으로 가정
         chess = new int[5,5];
+        
+        Init(5);
   
-        chess[0,0] = 1;
-        KnightTour(0,0,1);
+        chess[1,1] = 1;
+        KnightTour(1,1,1);
 
         for(int i = 0; i < chess.GetLength(0); i++){
             for(int j = 0; j < chess.GetLength(0); j++){
@@ -35,55 +42,74 @@ public class Lecture
         }
     }
 
+    //리스트 만들기를 건너뛸지를 체크할 배열
+    //한번 만들었으면 건너뛴다.
+    static bool[,] IsFinished;
+
     public static bool KnightTour(int x, int y, int count){
-    	int max = chess.GetLength(0);
+        int max = chess.GetLength(0);
+        //count 번호가 n*n개의 체스칸의 마지막 칸에 도달하면 종료
         if(count == max*max){
             chess[x,y] = count;
             return true;
-        }
-
-        int minVal = dx.Length;
-        int minIdx = 0;
-		
-		int nx;
-		int ny;
-        for(int i = 0; i < dx.Length; i++){
-            nx = x + dx[i];
-            ny = y + dy[i];
-            int val = 0;
-            if(ChkSafe(nx,ny)){
-                for(int j = 0; j < dx.Length; j++){
-                    int tx = nx + dx[j];
-                    int ty = ny + dy[j];
-                    if(ChkSafe(tx, ty)){
-                        val++;
+            }
+        
+        //바른스도르프 규칙을 이용하여 list를 만든다.
+        //c#에는 우선순위 큐가 없으므로 list를 sort함수로 정렬해준다.
+        if(!IsFinished[x,y]){
+            for(int i = 0; i < dx.Length; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                int val = 0;
+                if(Chk(nx, ny)){
+                    for(int j = 0; j < dx.Length; j++){
+                        int tx = nx + dx[i];
+                        int ty = ny + dy[i];
+                        if(Chk(tx, ty)){
+                            val++;
+                        }
                     }
-                }
 
-                if(minVal > val){
-                    minVal = val;
-                    minIdx = i;
+                    list[x,y].Add((nx, ny, val));
                 }
             }
+            list[x,y].Sort((x3, y3) => { if(x3.Item3 > y3.Item3) return 1; 
+                                       else if(x3.Item3 < y3.Item3) return -1;
+                                       return 0; });
+            IsFinished[x,y] = true;
         }
-        nx = x + dx[minIdx];
-        ny = y + dy[minIdx];
-        
-        if(ChkSafe(nx,ny)){
-            chess[x,y] = count;
-            //++count는 count 자체에 +1더하고 대입하는거고 count+1은 그냥 count에 1을 더한 식이다.
-            //엄연히 다르므로 헷갈리지말자
-            if(KnightTour(nx,ny,count+1)) return true;
-            else chess[x,y] = 0;
+        foreach(var u in list[x,y]){
+            int nx = u.Item1;
+            int ny = u.Item2;
+            if(Chk(nx, ny)){
+                chess[x,y] = count;
+                if(KnightTour(nx,ny,count+1)) return true;
+            	else{
+            		chess[x,y] = 0;
+            	}
+            }
         }
+        //새로 간 지점이 실패하면 돌아와서 다시 빈 공간을 체크해야 하므로 clear해준다.
+        list[x,y].Clear();
+        IsFinished[x,y] = false;
         return false;
     }
 
-    static bool ChkSafe(int x, int y)
+    static bool Chk(int x, int y)
     {
         if ((0 <= x && x < chess.GetLength(0)) && (0 <= y && y < chess.GetLength(0)) && (chess[x,y] == 0)){
             return true;
         }
         return false;
+    }
+    
+    static void Init(int n){
+    	list = new List<(int,int,int)>[n+1,n+1];
+        IsFinished = new bool[n+1,n+1];
+    	for(int i = 0; i <= n; i++){
+    		for(int j = 0; j <= n; j++){
+    			list[i,j] = new List<(int,int,int)>();
+    		}
+    	}
     }
 }
