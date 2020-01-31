70c1b3d4f2441ee144653aa7f3e194df2995fc72
SJGRID <sangjin256@naver.com>
2020-01-31 19:04:55 +0900

    Update MinimumCut.cs

--- Code_Implementation/Advanced Graph Algorithm/Maximum Flow/MinimumCut.cs
+++ Code_Implementation/Advanced Graph Algorithm/Maximum Flow/MinimumCut.cs
@@ -9,12 +9,16 @@ using System.Collections;
 
 public class Lecture 
 {
-    static List<(int,int)>[] adj;
-    
-    //최대 용량을 넣을 배열. a -> b 간선과 b -> a간선의 가중치를 더하면 adjFull[a,b]의 값이 나온다.
-    static int[,] adjFull;
-    
-    static bool[] visited;
+    //x -> y로 가는 간선만 추가해준다.
+    static List<int>[] adj;
+    //x -> y로 가는 간선 용량
+    static int[,] capacity;
+    //x -> y로 현재 흐르고 있는 유량
+    static int[,] flow;
+    //prev[i] = j는 i의 이전 노드가 j라는 뜻이다.
+    static int[] prev;
+
+    static int source, sink;
     public static void Main(string[] args) {
     	AdjInit(6);
         Add(1,2,5);
@@ -26,118 +30,89 @@ public class Lecture
         Add(3,6,5);
         Add(5,6,2);
 
+        source = 1; sink = 6;
         MaximumFlow();
-        
         Console.WriteLine(MinCut());
     }
 #region FordFulkerson
-    //기준값. 처음에는 적당히 큰 값
-    static int value = 20;  
-    
-    //용량 조절 알고리즘을 이용한 경로 찾기
-    //경로 찾기 결과가 false가 되면 더 이상 만들 수 있는 경로가 없는 것이므로
-    //알고리즘을 종료한다.
-    static List<(int,int,int)> temp = new List<(int,int,int)>();
-
+    static int maxFlow = 0;
     static int MaximumFlow(){
-        while(value > 0){
-            Array.Clear(visited, 0, visited.Length);
+        while(true){
+            Array.Clear(prev, 0, prev.Length);
             
-            if(!Dfs(1,6)){
-                value = value / 2;
-                continue;
+            Bfs();
+
+            if(prev[sink] == 0) break;
+
+            int min = 1000;
+            for(int i = sink; i != source; i = prev[i]){
+                min = Math.Min(min, capacity[prev[i],i] - flow[prev[i],i]);
             }
-            
-            
-            //선택한 경로에 포함된 간선의 가중치 중 가장 작은 가중치를 선택하고
-            //경로의 모든 간선에 이 값을 빼고, 반대간선에 이 값을 더함 
-            if(temp.Count != 0){
-                int min = 1000;
-                foreach(var u in temp){
-                    min = Math.Min(min, u.Item3);
-                }
-                if(min != 1000) SubWeight(min);
-                temp.Clear();
-            }
-            
+
+            SubWeight(min);
         }
-        
-        int max = 0;
-        foreach(var u in adj[6]){
-            max += u.Item2;
-        }
-        
-        return max;
+
+        return maxFlow;
     }
 
+    static Queue<int> q = new Queue<int>();
+    static void Bfs(){
+        q.Enqueue(source);
 
-    static bool Dfs(int s, int e){
-        if(s == e){
-            return true;
-        }
-        
-        visited[s] = true;
-        
-        foreach(var u in adj[s]){
-            if(visited[u.Item1]) continue;
-            if(u.Item2 >= value){
-                if(Dfs(u.Item1, e)){
-                    temp.Add((s, u.Item1, u.Item2));
-                    return true;
+        while(q.Count != 0){
+            int s = q.Dequeue();
+            foreach(var u in adj[s]){
+                if((prev[u] == 0) && (capacity[s,u] - flow[s,u] > 0)){
+                    prev[u] = s;
+                    q.Enqueue(u);
+                    if(u == sink) break;
                 }
-                //실패하면 다시 다른곳에서 돌아올 수 있으므로 u.Item1을 false해준다.
-                visited[u.Item1] = false;
             }
         }
-        return false;
+    }
+
+    static void SubWeight(int min){
+        for(int i = sink; i != source; i = prev[i]){
+            flow[prev[i],i] += min;
+            flow[i,prev[i]] -= min;
+        }
+        maxFlow += min;
     }
 
     static void AdjInit(int n){
-        adj = new List<(int,int)>[n+1];
-        adjFull = new int[n+1,n+1];
+        adj = new List<int>[n+1];
+        flow = new int[n+1,n+1];
+        capacity = new int[n+1,n+1];
+        prev = new int[n+1];
+        //최소컷 함수에서 사용
         visited = new bool[n+1];
-        for(int i = 1; i < n+1; i++){
-            adj[i] = new List<(int,int)>();
+        for(int i = 1; i <= n; i++){
+            adj[i] = new List<int>();
         }
     }
-    
-    //가중치를 최소값만큼 빼고 반대편 가중치를 올리는 함수
-    static void SubWeight(int min){
-        int x, y, w;
-        if(temp.Count != 0){
-            for(int i = temp.Count - 1; i >= 0; i--){
-                x = temp[i].Item1; y = temp[i].Item2; w = temp[i].Item3;
-                int XIndex = adj[x].IndexOf((y,w));
-                int YIndex = adj[y].IndexOf((x,(adjFull[x,y] - adj[x][XIndex].Item2)));
-                adj[x][XIndex] = (y,adj[x][XIndex].Item2 - min);
-                adj[y][YIndex] = (x,adj[y][YIndex].Item2 + min);
-            }
-        }
-    }
-    
+
     //a에서 b로 가는 가중치 w인 간선을 추가하는 함수
     //그 반대는 가중치를 0으로 해서 추가한다.
     static void Add(int a, int b, int w){
-        adj[a].Add((b,w));
-        adj[b].Add((a,0));
-        adjFull[a,b] = w;
+        adj[a].Add(b);
+        adj[b].Add(a);
+        capacity[a,b] = w;
+        capacity[b,a] = w;
     }
 #endregion
 
     //소스에서 갈수 있는 노드의 집합 A를 만들기 위한 리스트
     static List<int> list = new List<int>();
+    static bool[] visited;
     static int MinCut(){
         //먼저 집합 A를 만들어준다.
-        Dfs_MC(1,0);
+        Dfs_MC(source);
 
         int sum = 0;
         foreach(var s in list){
             foreach(var u in adj[s]){
-                if(!list.Contains(u.Item1)){
-                    //A에 속한 노드에서 A에 속하지 않은 노드로 가는 간선의 용량은
-                    //최대유량을 구할 때 다 썼으므로 당연히 0이며 그 반대 유량을 더하면
-                    //최소컷이다. 항상 최소컷 == 최대 유량이다.
-                    sum += adj[u.Item1][adj[u.Item1].IndexOf((s,adjFull[s,u.Item1]))].Item2;
+                if(!visited[u]){
+                    sum += (flow[s,u]);
                 }
             }
         }
@@ -145,13 +120,12 @@ public class Lecture
         return sum;
     }
 
-    static void Dfs_MC(int s,int e){
+    static void Dfs_MC(int s){
         list.Add(s);
         visited[s] = true;
-        Console.WriteLine(s);
         foreach(var u in adj[s]){
-            if((!visited[u.Item1]) && u.Item2 > 0){
-                Dfs_MC(u.Item1, s);
+            if((!visited[u]) && (capacity[s,u] - flow[s,u] > 0)){
+                Dfs_MC(u);
             }
         }
     }
