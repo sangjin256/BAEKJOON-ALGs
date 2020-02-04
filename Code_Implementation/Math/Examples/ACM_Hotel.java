e954fe96c6e1c716e275a7318b13137518166c27
SJGRID <sangjin256@naver.com>
2020-02-04 20:15:01 +0900

    Update ACM_Hotel.java

--- Code_Implementation/Math/Examples/ACM_Hotel.java
+++ Code_Implementation/Math/Examples/ACM_Hotel.java
@@ -30,37 +30,19 @@ import java.util.*;
 public class ACM_Hotel{
     public static void main(String[] args){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         try{
             int n = Integer.parseInt(br.readLine());
             for(int i = 0; i < n; i++){
                 int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
                 //층수와 그 층의 방번호를 따로 만들고 나중에 합친다.
-                int floor = arr[2];
-                int num = 1;
-                
-                if(floor%arr[0] == 0){
-                    sb.append(arr[0]);
-                    num = floor/arr[0];
-                    if(num < 10){
-                        sb.append("0" + num);
-                    }
-                    else sb.append(num);                    
-                }
-                else{
-                    floor = floor % arr[0];
-                    sb.append(floor);
-                    num += arr[2]/arr[0];
-                    //이 10보다 작으면 앞에 0을 붙여준다.
-                    if(num < 10){
-                        sb.append("0" + num);
-                    }
-                    else sb.append(num);
+                int num = (arr[2]/arr[0]) + 1;
+                int floor = arr[2]%arr[0];
+                if(arr[2]%arr[0] == 0){
+                    num = arr[2]/arr[0];
+                    floor = arr[0];
                 }
                 
-                System.out.println(sb);
-                
-                sb.setLength(0);
+                System.out.println(floor*100+num);
             }
 
         }catch(Exception e){e.printStackTrace();};
