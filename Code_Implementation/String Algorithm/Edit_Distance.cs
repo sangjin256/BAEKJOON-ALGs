1c41696baf3b75621725b685cf675289f4f36ca2
SJGRID <sangjin256@naver.com>
2020-02-07 19:37:03 +0900

    Update Edit_Distance.cs

--- Code_Implementation/String Algorithm/Edit_Distance.cs
+++ Code_Implementation/String Algorithm/Edit_Distance.cs
@@ -10,18 +10,24 @@ class Lecture{
         string str2 = Console.ReadLine();
 
         int[,] dp = new int[str1.Length+1, str2.Length+1];
-
+        //str1가 첫글자만 있을때와 str2가 첫글자만 있을때는 무조건 삽입해야만 하므로 미리 채워준다.
+        for(int i = 0; i <= str1.Length; i++){
+            dp[i,0] = i;
+        }
+        for(int i = 0; i <= str2.Length; i++){
+            dp[0,i] = i;
+        }
         for(int i = 1; i <= str1.Length; i++){
             for(int j = 1; j <= str2.Length; j++){
                 //각각 글자 삽입, 글자 삭제, 글자 수정(또는 유지)을 의미한다. 이중 가장 작은 값이 dp[i,j]에 들어간다.
-                dp[i,j] = Min(dp[i,j-1] + 1, dp[i-1,j] + 1, dp[i-1,j-1] + Cost(str1[i-1], str2[i-1]));
+                dp[i,j] = Min(dp[i,j-1] + 1, dp[i-1,j] + 1, dp[i-1,j-1] + Cost(str1[i-1], str2[j-1]));
             }
         }
         Console.WriteLine(dp[str1.Length, str2.Length]);
     }
 
     //두 값이 일치하면 0을 반환하고(유지) 일치하지 않으면 1을 반환한다.(수정)
-    public static int Cost(int a, int b){
+    public static int Cost(char a, char b){
         if(a == b) return 0;
         return 1;
     }
