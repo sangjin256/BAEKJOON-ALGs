386698edc73802fb4817c86187fa042006666042
SJGRID <sangjin256@naver.com>
2020-02-15 13:10:06 +0900

    Create TrieTest.java

---
+++ Code_Implementation/String Algorithm/TrieTest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class TrieTest{
+    public static void main(String[] args){
+        Trie trie = new Trie();
+        trie.insert("CANAL");
+        trie.insert("CANDY");
+        trie.insert("THE");
+        trie.insert("THERE");
+        trie.insert("FORGIVE");
+
+        //System.out.println(trie.delete("CANAL"));
+        System.out.println(trie.containsNode("CANAL"));
+
+        //System.out.println(trie.delete("FORGIVE"));
+        System.out.println(trie.containsNode("FORGIVE"));
+    }
+}
