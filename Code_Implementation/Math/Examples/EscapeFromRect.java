a9a42e1dd6485a29f60257d5692100c48e02e173
SJGRID <sangjin256@naver.com>
2020-02-24 13:01:45 +0900

    Update EscapeFromRect.java

--- Code_Implementation/Math/Examples/EscapeFromRect.java
+++ Code_Implementation/Math/Examples/EscapeFromRect.java
@@ -1,3 +1,9 @@
+// # 1085
+
+// 한수는 지금 (x, y)에 있다. 직사각형의 왼쪽 아래 꼭짓점은 (0, 0)에 있고, 오른쪽 위 꼭짓점은 (w, h)에 있다.
+
+// 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
 import java.util.*;
 class EscapeFromRect{
     public static void main(String[] args){
